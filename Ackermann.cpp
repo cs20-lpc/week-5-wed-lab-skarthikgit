f3c82abe5d2a7e43a713d59010aecc8a163ac205
Karthikeyan Shanmugam <skcodehack@gmail.com>
2026-02-23 15:19:17 -0800

    Init commit

--- Ackermann.cpp
+++ Ackermann.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
 using namespace std;
 
+/*
+The Ackermann function A(m, n) is defined recursively as follows:
+
+If m equals 0, then
+A(m, n) = n + 1
+
+If m is greater than 0 and n equals 0, then
+A(m, n) = A(m − 1, 1)
+
+If m is greater than 0 and n is greater than 0, then
+A(m, n) = A(m − 1, A(m, n − 1))
+*/
 // Ackermann function definition
 unsigned long long ackermann(unsigned int m, unsigned int n) {
- // TODO: Complete the code.
+    // TODO: Complete the code.
+    if (m == 0) {
+        return n + 1;
+    }
+
+    if (m > 0 && n == 0) {
+       return ackermann(m - 1, 1);
+    }
+
+    return ackermann(m - 1, ackermann(m, n - 1));
+
 }
 
 int main() {
